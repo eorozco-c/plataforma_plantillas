06bf10e283fcdca431ca60fbe9c30df3e0bdc46d
Ernesto Orozco C <ernestorozco94@gmail.com>
2022-10-03 11:07:35 -0300

    Se elimina de la vista el dashboard

--- apps/master/views.py
+++ apps/master/views.py
@@ -13,11 +13,12 @@ def index(request):
         if Usuario.objects.count() == 0:
             return redirect("usuarios:registrar")
         if request.user.is_authenticated:
-            return redirect("master:menu")
+            return redirect("plantillas:index")
     return redirect("usuarios:login")
 
 @login_required(login_url='/')
 def menu(request):
+    return redirect("plantillas:index")
     ramUsage = round(psutil.virtual_memory().percent)
     ramAvailable = round(psutil.virtual_memory().available * 100 / psutil.virtual_memory().total)
     hdd = psutil.disk_usage('/')
